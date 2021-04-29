cff7d0835d4ec48bd35f4d3c6d2124e6dadf140b
Sheila Anguiano <sheila.anguiano@gmail.com>
2021-04-29 16:21:00 -0700

    counter component functionality

--- app.js
+++ app.js
@@ -30,15 +30,45 @@ const Header = ( props ) => {
   );
 }
 
-const Counter = (props) => {
-  return (
-    <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <span className="counter-score"> { props.score } </span>
-        <button className="counter-action increment"> + </button>
-      </div>
+class Counter extends React.Component {
 
-  )
+  // This is the same as just declaring state
+  // but that is not currently supported in all browsers 
+  
+  // constructor() {
+  //   super()
+  //   this.state= {
+  //     score: 0
+  //   };
+  // }
+
+  state = {
+    score: 0
+  };
+
+  incrementScore = () => {
+    this.setState({
+      score: this.state.score +1
+    });
+  }
+
+  decrementScore = () => {
+    this.setState({
+      score: this.state.score -1
+
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="counter">
+          <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+          <span className="counter-score"> { this.state.score } </span>
+          <button className="counter-action increment" onClick={this.incrementScore} > + </button>
+        </div>
+    );
+  }
 }
 
 const Player = ( props ) => {
@@ -47,7 +77,7 @@ const Player = ( props ) => {
       <span className ="player-name">
         { props.name }
       </span>
-      <Counter score={ props.score }/>      
+      <Counter />      
     </div>
   )
 }
@@ -62,7 +92,6 @@ const App = (props) => {
       { props.initialPlayers.map( player =>  
         <Player  
           name= {player.name} 
-          score= {player.score}
           key= {player.id.toString()}    
           />
         )
